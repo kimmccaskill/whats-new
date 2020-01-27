d4a58b0321c32b256de9641fddcbdb27da7a7395
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-27 16:40:53 -0700

    Add local news cards

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle.js'
 
-// NEWSCONTAINER COMPONENT CODE GOES HERE
+function NewsContainer(props) {
+  const localNews = props.localNews.map(article => <NewsArticle key={article.id} image={article.img} headline={article.headline} description={article.description} url={article.url}/>)
+
+  return (
+    <div className='news-container'>
+      {localNews}
+    </div>
+  )
+}
 
 export default NewsContainer;
