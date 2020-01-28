25937912bfacfe7b94590aa3abc6286156790c1d
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-28 14:35:40 -0700

    Add all data files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
+import entertainment from '../../data/entertainment';
+import health from '../../data/health';
 import local from '../../data/local';
+import science from '../../data/science';
+import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer.js'
+import Menu from '../Menu/Menu.js'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      categories: {
+        Entertainment : {data: entertainment},
+        Health: {data: health},
+        Local: {data: local},
+        Science: {data: science},
+        Technology: {data: technology}},
+      articleShown: {data: local}
     }
+  }
+
+  changeCategory = (category) => {
+    this.setState({articleShown: {data: this.state.categories[category].data}})
   }
 
   render () {
     return (
       <div className="app">
-        <NewsContainer localNews={this.state.local}/>
+        <Menu changeCategory={this.changeCategory} data={this.state}/>
+        <NewsContainer news={this.state.articleShown.data}/>
       </div>
     );
   }
