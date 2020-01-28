3ad53523c0fa27dc782bc3921aab321e37ff3c3b
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-28 14:37:00 -0700

    Add update articles method

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,29 @@
 import React from 'react';
 import './Menu.css'
 
-// MENU COMPONENT CODE GOES HERE
+class Menu extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // articleShown: ''
+    }
+  }
+
+  updateArticlesShown = (event) => {
+    // console.log(event.target.value.toLowerCase())
+    this.props.changeCategory(event.target.value)
+  }
+
+  render() {
+    let navItems = Object.keys(this.props.data.categories)
+    navItems = navItems.map(item => <button className="menu-btns" type="button" value={item} onClick={this.updateArticlesShown}>{item}</button>)
+    return(
+      <aside className='nav-bar'>
+        <h1>What's New?</h1>
+        {navItems}
+      </aside>
+    )
+  }
+}
 
 export default Menu;
