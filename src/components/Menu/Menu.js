98d5c9421867de2c1ce33cc7694b3f7fbae26ef6
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-29 16:38:26 -0700

    Add conditional for null objects

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,29 +1,19 @@
 import React from 'react';
 import './Menu.css'
 
-class Menu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // articleShown: ''
-    }
+const Menu = (props) => {
+  const updateArticlesShown = (event) => {
+    props.changeCategory(event.target.value)
   }
+  let navItems = props.data.categories ? Object.keys(props.data.categories) : null;
+  navItems = navItems ? navItems.map(item => <button className="menu-btns" type="button" value={item} onClick={updateArticlesShown}>{item}</button>) : null;
 
-  updateArticlesShown = (event) => {
-    // console.log(event.target.value.toLowerCase())
-    this.props.changeCategory(event.target.value)
-  }
-
-  render() {
-    let navItems = Object.keys(this.props.data.categories)
-    navItems = navItems.map(item => <button className="menu-btns" type="button" value={item} onClick={this.updateArticlesShown}>{item}</button>)
-    return(
-      <aside className='nav-bar'>
-        <h1>What's New?</h1>
-        {navItems}
-      </aside>
-    )
-  }
+  return(
+    <aside className='nav-bar'>
+      <h1>What's New?</h1>
+      {navItems}
+    </aside>
+  )
 }
 
 export default Menu;
