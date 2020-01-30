e0b1e7b76153c59f1a1e0e217f29a96bbf63a99b
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-29 22:01:41 -0700

    Add search form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,6 +7,7 @@ import technology from '../../data/technology';
 import './App.css';
 import NewsContainer from '../NewsContainer/NewsContainer.js'
 import Menu from '../Menu/Menu.js'
+import SearchForm from '../SearchForm/SearchForm.js'
 
 class App extends Component {
   constructor() {
@@ -28,10 +29,13 @@ class App extends Component {
 
   render () {
     return (
-      <div className="app">
+      <main className="app">
         <Menu changeCategory={this.changeCategory} data={this.state}/>
-        <NewsContainer news={this.state.articleShown.data}/>
-      </div>
+        <section className="main-section">
+          <SearchForm />
+          <NewsContainer news={this.state.articleShown.data}/>
+        </section>
+      </main>
     );
   }
 }
