5f2610e741a0d66c1985665ffc4f3190df2803cd
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-29 23:34:09 -0700

    Add filter articleS

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchInput: ''}
+  }
+
+  updateChange = (event) => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  submitSearch = () => {
+    this.props.filterArticles(this.state.searchInput)
+  }
+
+  render() {
+    return (
+      <form className="nav-bar">
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search for article here"
+          value={this.state.searchInput}
+          onChange={this.updateChange}
+        />
+        <button
+          type="button"
+          className="search-btn"
+          onClick={this.submitSearch}
+        >Search</button>
+      </form>
+    )
+  }
+}
 
 export default SearchForm;
