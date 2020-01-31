3db039644f56fbed1950e2cb422b6a94503a18c9
Kim McCaskill <kimmccaskill92@gmail.com>
2020-01-31 10:42:22 -0700

    Destructure props in NewsArticle

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './NewsArticle.css';
 // Each NewsArticle card will display: the image, the headline, the description, and the url (see the screenshot above for an idea).
 
-function NewsArticle(props) {
+function NewsArticle({ image, headline, description, url }) {
   return (
     <div className='article-card'>
-      <img className="card-image" src={props.image} alt={props.headline} />
+      <img className="card-image" src={image} alt={headline} />
       <div>
-        <h3>{props.headline}</h3>
-        <p className="card-description">{props.description}</p>
-        <a href={props.url}>Check it out</a>
+        <h3>{headline}</h3>
+        <p className="card-description">{description}</p>
+        <a href={url}>Check it out</a>
       </div>
     </div>
   )
